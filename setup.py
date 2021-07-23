1865126551a2e9661af85d82d9f037a7e971c218
uttu540 <utkarsh.agrawal55@gmail.com>
2021-07-23 11:17:53 +0530

    setup commit

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name = 'distributions',
+      version='0.1',
+      description='Gaussian distribution',
+      packages=['distributions'],
+      zip_safe=False)
